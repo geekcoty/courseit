3816c21fcee320a53be20eb1f26532dd9ff9412b
geekcoty <ccareal@gmail.com>
2020-04-24 10:44:22 -0300

    save

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-Dada una variable "x" la cual contiene un numero, determinar si ese numero es positivo o negativo. Si es cero, mostrar otro mensaje
+//Dada una variable "x" la cual contiene un numero, imprimir en consola todos los numeros que esten entre el valor de la variable y 0.
+
+//Ejemplo: Si la variable X es 3, imprimir en pantalla 3, 2, 1 y 0
+
+let num =6
+for (let i=0 ;i <= num 6; i++){
+    console.log (i)
+}
