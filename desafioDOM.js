f90a698ad23b0384413e968ebfe6dfc7c5492630
geekcoty <ccareal@gmail.com>
2020-05-15 11:18:27 -0300

    save

--- desafioDOM.js
+++ desafioDOM.js
@@ -1,4 +1,13 @@
-function clean  () => {
+function addItem() {
+    const itemList = document.getElementById("list");
+    const newItem = document.createElement("li");
+    const text = document.createTextNode(itemNuevo.value);
+    newItem.appendChild(text);
+    list.appendChild(newItem);
+  }
 
+  const function  removeItem() {
+    document.getElementById("list").innerHTML = null;
+  }
 
-}
+  
