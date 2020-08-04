6b1a3f9b116c77aef1ca9f9783b47c10695372cf
geekcoty <ccareal@gmail.com>
2020-08-04 19:58:43 -0300

    save changes

--- js/challenges/index.js
+++ js/challenges/index.js
@@ -1,8 +1,9 @@
 function numOrStr(value) {
 
   if (typeof value === "number") {
-    alert("El dato que pasaste es un número")
+    return (typeof value)
   } else if (typeof value === "string") {
-    alert("El dato que pasaste es un string")
+    return(typeof value)
   }
 }
+
