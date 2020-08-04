f2865f5aa498dc5af71389d734977c2cac29b2cb
geekcoty <ccareal@gmail.com>
2020-08-04 16:44:44 -0300

    new files

---
+++ js/challenges/index.js
@@ -0,0 +1,8 @@
+function numOrStr(value) {
+
+  if (typeof value === "number") {
+    alert("El dato que pasaste es un número")
+  } else if (typeof value === "string") {
+    alert("El dato que pasaste es un string")
+  }
+}
