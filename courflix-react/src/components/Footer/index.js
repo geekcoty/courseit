8128f1cd5fcab36680b7519da539c73acf0308e5
geekcoty <ccareal@gmail.com>
2020-08-20 09:37:47 -0300

    changes

--- courflix-react/src/components/Footer/index.js
+++ courflix-react/src/components/Footer/index.js
@@ -1,24 +1,21 @@
 import React from "react"
 import "./style.scss";
 
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 class Footer extends React.Component {
   render(){
     return (
-
       <div className="footer-wrapper">
         <span className="credit">
-
-          Made with LAV by <a href="#">Coty</a>
-
+          Made with <FontAwesomeIcon icon={faHeart} /> by{" "}
+          <a href="https://twitter.com/rememberplut" className="twitter-handle" target="_new">
+            Coty
+          </a>
         </span>
-
       </div>
-
-
-
-
-    )
+    );
   }
 }
 
--- courflix-react/src/components/Hero/index.js
+++ courflix-react/src/components/Hero/index.js
@@ -1,19 +1,31 @@
-import React from "react"
+import React from "react";
 import "./style.scss";
 
-
 class Hero extends React.Component {
-  render (){
+  render() {
     return (
-      
       <div className="hero-wrapper">
-        <p> LET ME BE YOUR HERO BABY</p>
+
+        <div className="hero-title">
+          <span> TITULO</span>
+        </div>
+
+        <div className="hero-details">
+          <span> DETALLES </span>
+        </div>
+        <div className="hero-synopsis">
+          <span> DESCRIPCIÓN </span>
+        </div>
+
+        <div className="hero-buttons">
+
+          <button className="play-buttons"> HOLA</button>
+          <button className="play-buttons"> HOLA </button>
+          
+        </div>
       </div>
-
-
-
-    )
+    );
   }
 }
 
-export default Hero
+export default Hero;
