cc025869e2407de956720625a8db37a76c2f090a
geekcoty <ccareal@gmail.com>
2020-11-01 18:36:01 -0300

    test

--- gistgatitos/src/App.js
+++ gistgatitos/src/App.js
@@ -2,20 +2,34 @@ import React from "react"
 class App extends React.Component {
   constructor(props) {
     super(props);
-console.log ("soy un constructor")
+    this.state = {
+      catPic: "",
+    };
   }
-  componentDidMount() {
-    console.log("soy un componentDidMount")
+
+  async componentDidMount() {
+    const getCat = await fetch(" https://api.thecatapi.com/v1/images/search");
+    const getCatJson = await getCat.json();
+
+    this.setState({
+      catPic: getCatJson[0].url
+    });
   }
-  
-  render () {
-    console.log("soy un render")
-  return (
-    <div className="App">
-     <p> Hola </p>
-     <p> Chau </p>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <p> DATO MUY IMPORTANTE</p>
+        {this.state.catPic ? (
+        <img src={this.state.catPic} alt="gatite" />
+        ) : (
+          <p> Cargando</p>
+        )}
+
+        {!this.state.catPic && (
+          <p> No hay ninguna imagen de gato </p>
+        )}
+      </div>
+    );
   }
 }
 
