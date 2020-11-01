113c8119aa5ed3f3f76b25d492b3c0e680fb5ba9
geekcoty <ccareal@gmail.com>
2020-11-01 15:20:16 -0300

    test

--- gist2/src/App.js
+++ gist2/src/App.js
@@ -2,17 +2,36 @@ import React from "react"
 import './App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      firstNum: 0,
+      secondNum: 0,
+      result:0
+    }
+  }
   handleChange (e) {
-    return (
-      console.log ("hola")
-    )
+    const {name, value} = e.target
+    this.setState ({ [name] : value
+    })
+  }
+
+  handleSum (){
+
+    const {firstNum,secondNum} = this.state
+    this.setState ({
+      result: parseInt(firstNum) +parseInt(secondNum)
+    })
   }
   render () {
   return (
     <div className="App">
-      <input type="text" onChange = {(e) => this.handleChange(e)}/>
-      <input type="text" />
-      <button> sumar </button>
+      <input name= "firstNum" type="text" onChange = {(e) => this.handleChange(e)}/>
+      <input name="secondNum" type="text" onChange = {(e) => this.handleChange(e)}/>
+
+      <button onClick ={() => this.handleSum()}> sumar </button>
+
+      <p> El resultado es : {this.state.result}</p>
   
     </div>
   )
