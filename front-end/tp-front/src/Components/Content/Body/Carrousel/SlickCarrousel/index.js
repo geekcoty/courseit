556b287b7734c0c075507ab12a479992da14fd7c
geekcoty <ccareal@gmail.com>
2023-01-30 08:54:07 -0300

    final tweeks

--- front-end/tp-front/src/Components/Content/Body/Carrousel/SlickCarrousel/index.js
+++ front-end/tp-front/src/Components/Content/Body/Carrousel/SlickCarrousel/index.js
@@ -15,21 +15,28 @@ class SlickCarrousel extends React.Component {
       slidesToScroll: 1,
       accesibility: true,
       responsive: [
-     {breakpoint: 768,
-      settings: {
-        slidesToShow: 4,
-        slidesToScroll: 4,
-      }
-    },
-    {breakpoint: 480,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2,
-      }
-    }
- ]
-
-
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+          },
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 4,
+            slidesToScroll: 4,
+          },
+        },
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 5,
+            slidesToScroll: 5,
+          },
+        }
+      ],
     };
   }
   render() {
